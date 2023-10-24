113a38b65154799514aaad76d4b1ef1a7336474f
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2023-10-24 18:01:06 +0900

    feat: product

--- router_bssr.js
+++ router_bssr.js
@@ -1,6 +1,8 @@
 const express = require("express"); 
 const router_bssr = express.Router();      //exprees ni ichidan routerni olob chiqamiz       //turli xil api larni addresslari qaysi api bilan kelgan addresslarni qayerga borishini xal qiladi 
 const restaurantController = require('./contollers/restaurantController');
+const productController = require('./contollers/productController');
+const uploader_product = require("./utils/upload-multer")("products");
 
 /************************************************
  *              BSSR EJS          * 
@@ -21,5 +23,10 @@ router_bssr.get("/logout", restaurantController.logout);
 router_bssr.get("/check-me", restaurantController.checkSessions);
 
 router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
+router_bssr.post("/products/create",
+restaurantController.validateAuthRestaurant,
+uploader_product.array("product_images", 5),
+productController.addNewProduct);
+router_bssr.post("products/edit/:id", productController.updateChosenProduct); 
 
 module.exports = router_bssr;
