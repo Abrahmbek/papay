d46c09922b746e5ece69a7cc2283f3b277ba52e7
Abrahmbek <145168869+Abrahmbek@users.noreply.github.com>
2023-12-12 04:04:13 +0900

    feat: 79 80 81 done

--- router.js
+++ router.js
@@ -75,4 +75,10 @@ router.post("/community/create",
 memberController.retrieveAuthMember,
  communityController.createArticle
 );
+
+router.get("/community/articles",
+memberController.retrieveAuthMember,
+ communityController.getMemberArticles
+);
+
 module.exports = router;
